1d2b79c90d68f6a9b7413cb8a87b69bb8515d370
Jose Rubio <jose.rubio@gmail.com>
2021-01-04 11:34:55 +0100

    Network cache.

--- wrapper/NetworkLoader.py
+++ wrapper/NetworkLoader.py
@@ -6,22 +6,27 @@ class NetworkLoader:
 	def __init__(self, output_dir):
 		self.output_dir = output_dir
 		os.makedirs(self.output_dir, exist_ok=True)
+		self.cache = {}
 
+	# TODO: Add cache.
 	# TODO: Make this compatible with other sources than GDrive.
 	# TODO: Make saved_network_name optional
 	# Currently "network_path" is expected to be a GDrive file id.
 	def load(self, network_path, saved_network_name, output_dir=None):
-		url = f'https://drive.google.com/uc?id={network_path}'
-
-		if output_dir is not None:
-			dir = output_dir
+		if self.cache[network_path]:
+			pickle_file = self.cache[network_path]
 		else:
-			dir = self.output_dir
+			url = f'https://drive.google.com/uc?id={network_path}'
 
-		output = f'{dir}/{saved_network_name}.pkl'
+			if output_dir is not None:
+				dir = output_dir
+			else:
+				dir = self.output_dir
 
-		pickle_file = gdown.download(url, output, False)
-		print('pickle_file', pickle_file)
+			output = f'{dir}/{saved_network_name}.pkl'
+
+			pickle_file = gdown.download(url, output, False)
+
 		with open(pickle_file, 'rb') as pf:
 			_G, _D, Gs = pickle.load(pf)
 
