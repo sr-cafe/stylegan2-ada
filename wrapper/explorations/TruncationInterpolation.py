9d7d3e46e7510e53b740dc345765ede5ea90d5f9
Jose Rubio <jose.rubio@gmail.com>
2021-01-05 12:26:00 +0100

    Truncation interp.

--- wrapper/explorations/TruncationInterpolation.py
+++ wrapper/explorations/TruncationInterpolation.py
@@ -3,19 +3,19 @@ from ..Latent import Latent
 from .BaseExploration import BaseExploration
 from .Easings import *
 
-class LinearInterpolation(BaseExploration):
+class TruncationInterpolation(BaseExploration):
 	def __init__(self, styleGanWrapper):
 		BaseExploration.__init__(self, styleGanWrapper)
 
-	def run(self, start, end, steps, easing=LinearInOut):
+	def run(self, seed, start, end, steps, easing=LinearInOut):
 		step_inc = 1.0 / steps
 		steps_num = np.arange(0, 1, step_inc)
 		images = []
-		truncation = start.truncation_psi
-		interpolator = easing(start.vector, end.vector, 1)
+
+		interpolator = easing(start, end, 1)
 
 		for alpha in steps_num:
-			vector = interpolator.ease(alpha)
-			images.append(self.styleGanWrapper.from_z_vector(vector, truncation))
+			truncation = interpolator.ease(alpha)
+			images.append(self.styleGanWrapper.from_latent(Latent.from_seed(seed, truncation)))
 
 		return images
